e2c46181730a2be505366e1004a13e19a371dbdb
YotaGod <muh.ilhamrizalulfath02@gmail.com>
2026-05-29 17:25:04 +0700

    chore: remove unused test files test.js and test2.js

--- test.js
+++
@@ -1,3 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const s = createClient('https://ntayegtnngnnepspnakv.supabase.co', 'sb_publishable_CHeRW6bn0Y2ouIpKKtrs3Q_tYnRR6iy');
-s.from('user_profiles').select('*, city:cities!region_city_id(name)').limit(1).then(console.log).catch(console.error);
